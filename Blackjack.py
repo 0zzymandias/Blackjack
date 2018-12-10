6b0c45de640eb402d22f42b31cd15208501c4134
0zzymandias <30847435+0zzymandias@users.noreply.github.com>
2018-12-09 18:37:53 -0600

    Implemented hands+card drawing

--- Blackjack.py
+++ Blackjack.py
@@ -1,7 +1,7 @@
 ##Blackjack exercise.
 
 import random
-x = 1
+
 CardNum = {2, 3, 4, 5, 6, 7, 9, 'Jack', 'King', 'Queen', 'Ace'}
 CardSuit = {'Clubs','Hearts', 'Diamonds','Spades'}
 #Reference deck
@@ -11,22 +11,23 @@ for x in CardNum:
     for y in CardSuit:
         originaldeck.append(str(x) + ' of ' + str(y))
 
-productiondeck = originaldeck
+def Hit():
+        hit = (random.choice(originaldeck))
+        originaldeck.remove(hit)
+        print(hit)
+        return hit
+print('')
+print('Dealer has:')
+dealerhand = Hit()
+print("and a hidden card")
+print('')
 
-
-
+print("You have:")
+playerhand = Hit()
+playerhand = Hit()
 print("")
-print("")
-print("")
 
-def DrawPlayerCards():
-    playercard1 = (random.choice(productiondeck))
-    playercard2 = (random.choice(productiondeck))
-    productiondeck.remove(playercard1)
-    productiondeck.remove(playercard2)
-    print(playercard1)
-    print(playercard2)
+print(originaldeck)
 
-for x in productiondeck:
-    print(x)
 
+print('=')
