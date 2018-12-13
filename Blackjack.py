5670031e40381d17cd862f8ea10a31b13ab4ef34
0zzymandias <30847435+0zzymandias@users.noreply.github.com>
2018-12-12 19:31:41 -0600

    Added counting function, began card-punching function

--- Blackjack.py
+++ Blackjack.py
@@ -2,9 +2,11 @@
 
 import random
 
-CardNum = {2, 3, 4, 5, 6, 7, 9, 'Jack', 'King', 'Queen', 'Ace'}
+#Card combinations in their own dictionaries
+CardNum = {2, 3, 4, 5, 6, 7, 9, 10, 'Jack', 'King', 'Queen', 'Ace'}
 CardSuit = {'Clubs','Hearts', 'Diamonds','Spades'}
-#Reference deck
+
+#Reference deck, zeroed
 originaldeck = []
 
 #Zeroed hand values
@@ -14,21 +16,66 @@ playerhandvalue = 0
 dealerhandvalue = 0
 
 
+#Add all card combinations to a list
 for x in CardNum:
     for y in CardSuit:
         originaldeck.append(str(x) + ' of ' + str(y))
 
+
+
+#Player hits to their own lists.
 def PlayerHit():
         hit = (random.choice(originaldeck))
         originaldeck.remove(hit)
         playerhand.append(hit)
         print(hit)
+#Dealer hits to their own lists.
 def DealerHit():
         hit = (random.choice(originaldeck))
         originaldeck.remove(hit)
         dealerhand.append(hit)
         print(hit)
 
+#Find all cards and assign value to the hand, assuming aces can go both high and low.
+#Stringvalues 
+def HandValueCounter(stringvalues, handvalue_numeric):
+        if 'Ace' in stringvalues and handvalue_numeric < 11:
+                stringvalues.replace('Ace','Counted',1)
+                return 11
+        if 'Ace' in stringvalues and handvalue_numeric >= 11:
+                return 1
+        if 'Queen' in stringvalues:
+                return 10
+        if 'King' in stringvalues:
+                return 10
+        if 'Jack' in stringvalues:
+                return 10
+        if '10' in stringvalues:
+                return 10
+        if '9' in stringvalues:
+                return 9
+        if '8' in stringvalues:
+                return 8
+        if '7' in stringvalues:
+                return 7
+        if '6' in stringvalues:
+                return 6
+        if '5' in stringvalues:
+                return 5
+        if '4' in stringvalues:
+                return 4
+        if '3' in stringvalues:
+                return 3
+        if '2' in stringvalues:
+                return 2
+
+
+#Because strings are immutable in Python, we have to "punch" used cards from the original reference hand.
+#Else, we will count only the highest card twice.
+
+
+
+#Actual game thus far
 print('')
 print('Dealer has:')
 DealerHit()
@@ -42,13 +89,17 @@ print("")
 
 print('=')
 
+#The post-hit dealer and player hands, in string format.
+string_dealerhand = str(dealerhand)
+string_playerhand = str(playerhand)
 
 
-def HandValueCounter(hand, handvaluecounter):
-        if ('Ace' in hand) and (handvaluecounter < 11):
-                handvaluecounter+=11
-        elif ('Ace' in hand) and (handvaluecounter >= 11):
-                        handvaluecounter+=1
-        print(handvaluecounter)
+playerhandvalue += int(HandValueCounter(string_playerhand, playerhandvalue))
+playerhandvalue += int(HandValueCounter(string_playerhand, playerhandvalue))
 
-HandValueCounter(playerhand, playerhandvalue)
+print('Playerhand')
+print(playerhand)
+print('Playerhand -> To string')
+print(string_playerhand)
+print('Player hand numerical value')
+print(playerhandvalue)
