b60474328b857bc9c0b512c38e94fe1d63a10f04
0zzymandias <30847435+0zzymandias@users.noreply.github.com>
2018-12-18 18:56:02 -0600

    Added soft ace

--- Blackjack.py
+++ Blackjack.py
@@ -1,10 +1,12 @@
 ##Blackjack exercise.
 
 import random
+import time
 
 playerbalance = 500
 bet = 25
 #Card combinations in their own dictionaries
+#
 CardNum = {2, 3, 4, 5, 6, 7, 9, 10, 'Jack', 'King', 'Queen', 'Ace'}
 CardSuit = {'Clubs','Hearts', 'Diamonds','Spades'}
 
@@ -19,6 +21,8 @@ dealerhandvalue = 0
 
 playersplitchecker1 = 0
 playersplitchecker2 = 0
+
+
 
 resolved = 'false'
 
@@ -61,7 +65,7 @@ def HandValueCounter(drawncard, handvalue_numeric):
         if '2' in drawncard:
                 return 2
 
-#Player hits to their own lists.
+#Player hits to their own lists, checking for splits.
 def PlayerHit(splitchecker):
         hit = (random.choice(originaldeck))
         originaldeck.remove(hit)
@@ -77,6 +81,17 @@ def PlayerHit(splitchecker):
         splitchecker = int(HandValueCounter(hit, playerhandvalue))
         print(hit)
         return splitchecker
+
+def PlayerHit2():
+        hit = (random.choice(originaldeck))
+        originaldeck.remove(hit)
+        
+        #Call the player hand from above. Add the actual value of the card to it.
+        global playerhandvalue
+        playerhandvalue += HandValueCounter(hit, playerhandvalue)
+        
+        playerhand.append(hit)
+        print(hit)
 #Dealer hits to their own lists.
 def DealerHit():
         hit = (random.choice(originaldeck))
@@ -86,8 +101,6 @@ def DealerHit():
         dealerhand.append(hit)
         print(hit)
 
-
-
 #Actual game thus far
 print('')
 print('Dealer has:')
@@ -95,7 +108,7 @@ DealerHit()
 print("and a hidden card")
 print('')
 print('Dealer count estimate: ' + str(dealerhandvalue+10)) #Add a ten to the dealer count estimate.
-print('(Always assume the dealer is hiding a 10.)') 
+print('(Assume the dealer is hiding a 10, but not always. (House edge in always making this assumption: 10.03%))') 
 print('')
 
 
@@ -104,13 +117,9 @@ playersplitchecker1 = (PlayerHit(playersplitchecker1))
 playersplitchecker2 = (PlayerHit(playersplitchecker2))
 print('Total:')
 print(playerhandvalue)
-print('')
 
-
-
-#Determine first-card events.
-evenmoneyevent = 0 
 answeredcorrectly = 'false'
+#Before any action is taken, prompt even-money event in the case of dual-blackjacks.
 if int(playerhandvalue) == 21 and (int(dealerhandvalue+10) == 21 or int(dealerhandvalue+10) == 20):
         evenmoneyevent = 1
         print('Even money? (Yes or no)')
@@ -129,6 +138,73 @@ if int(playerhandvalue) == 21 and (int(dealerhandvalue+10) == 21 or int(dealerha
                 print('+'+ str(bet))
         if x == 'no':
                 evenmoneyevent = 0
+                print('')
+                print('Dealer flips over their card...')
+                time.sleep(0.5)
+                DealerHit()
+        if dealerhandvalue == playerhandvalue:
+                print('The game ends in a push.')
+print('')
+
+
+#Current issues with Soft 21 busting. (K+A)
+
+hand = 'unresolved'
+handace = ''
+
+if 'Ace' in playerhand:
+        handace = 'Soft'
+
+while hand != 'resolved':
+        print('')
+        print('What will you do?')
+        print('Current hand value:')
+        print(str(handace) + ' ' + str(playerhandvalue))
+        print('Hit (1), Stay (2), Split (3), Double (4)')
+        action = input('#')
+        
+        #Hit
+        if action == '1':
+                #Draw card
+                PlayerHit2()
+                #Player busts on hard card
+                if playerhandvalue > 21 and handace == 'Hard':
+                        print(playerhandvalue)
+                        print('Busted!')
+                        hand = 'resolved'
+                #Player has actions remaining
+                if playerhandvalue < 21:
+                        print('What will you do?')
+                #Player uses up soft ace
+                if playerhandvalue > 21 and handace == 'Soft':
+                        playerhandvalue = playerhandvalue - 10
+                        handace = 'Hard'
+        #Stand
+        #if action = '2':
+        #Split
+        #if action = '3':
+        #Double
+        #if action = '4':
+
+
+#Note to self: you may want to create card slots for each hand. 6 card Charlie is statistically improbable.
+
+action = input('#')
+
+while x == x:
+        print('Your hand: '+ str(playerhandvalue))
+        print('Dealers hand: '+ str(dealerhandvalue))
+        print('What will you do?')
+        print('Hit (1), Stay (2), Split (3), Double (4)')
+        action = input('#')
+        if action == '1':
+                PlayerHit(playersplitchecker1)
+                print(playerhandvalue)
+                
+#Determine first-card events.
+evenmoneyevent = 0 
+answeredcorrectly = 'false'
+
                 
 print('End!')
 
@@ -144,5 +220,4 @@ if evenmoneyevent == 0 and (int(dealerhandvalue+10) == 21 or int(dealerhandvalue
 if int(playersplitchecker1) == int(playersplitchecker2):
         print('You can split.')
 
-print('Which action will you take?')
-print('Hit (1), Stay (2), Split (3), or Double?(4)')
+
